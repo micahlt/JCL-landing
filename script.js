a9d791c4cd4e51d417aa77fbb60738007c3c71cf
Micah Lindley <micahincorporated@gmail.com>
2020-01-30 14:32:41 +0000

    Final product, minus information!

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-    document.getElementById("navbar").style.height = "100px";
+    document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 85%)";
+    document.getElementById("navbar").style.boxShadow = "black 0 0 5px";
+    document.getElementById("navHeader").style.opacity = "1";
   } else {
-    document.getElementById("navbar").style.height = "0px";
+    document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0%)";
+    document.getElementById("navbar").style.boxShadow = "black 0 0 0px";
+    document.getElementById("navHeader").style.opacity = "0";
   }
 }
